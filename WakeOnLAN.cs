9bf9d1850c223ef4d86ea076845ce07202b97113
Dlegend123 <marklegen@gmail.com>
2022-03-09 09:24:26 -0500

    initial commit

---
+++ WakeOnLAN.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ultimate_Port_Scanner
+{
+    public class WakeOnLAN : UdpClient
+    {
+        //we derive our class from a standart one
+        public WakeOnLAN() : base()
+        {
+
+        }
+        //this is needed to send broadcast packet
+        public void SetClientToBrodcastMode()
+        {
+            if (this.Active)
+                this.Client.SetSocketOption(SocketOptionLevel.Socket,
+                                          SocketOptionName.Broadcast, 0);
+        }
+        //now use this class
+        //MAC_ADDRESS should  look like '013FA049'
+        private void WakeFunction(string MAC_ADDRESS,int port)
+        {
+            WakeOnLAN client = new WakeOnLAN();
+            client.Connect(new
+               IPAddress(0xffffffff),  //255.255.255.255  i.e broadcast
+               port);
+            client.SetClientToBrodcastMode();
+            //set sending bites
+            int counter = 0;
+            //buffer to be send
+            byte[] bytes = new byte[1024];   // more than enough :-)
+                                             //first 6 bytes should be 0xFF
+            for (int y = 0; y < 6; y++)
+                bytes[counter++] = 0xFF;
+            //now repeate MAC 16 times
+            for (int y = 0; y < 16; y++)
+            {
+                int i = 0;
+                for (int z = 0; z < 6; z++)
+                {
+                    bytes[counter++] =
+                        byte.Parse(MAC_ADDRESS.Substring(i, 2), NumberStyles.HexNumber);
+                    i += 2;
+                }
+            }
+
+            //now send wake up packet
+            int reterned_value = client.Send(bytes, 1024);
+        }
+    }
+}
